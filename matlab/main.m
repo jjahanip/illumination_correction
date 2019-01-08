23c4e42ae75591cf967963ffe67bcb5ed5755aa9
jjahanip <jahandar.jahani@gmail.com>
2019-01-07 19:44:45 -0600

    add histogram equlizer to Matlab version

--- matlab/main.m
+++ matlab/main.m
@@ -21,11 +21,20 @@ se = strel('disk',disk_size);
 
 image_fnames = dir(fullfile(input_dir, '*.tif'));
 for i=1:size(image_fnames, 1)
+    % read image and get the histogram of the original image
     im = imread(fullfile(input_dir, image_fnames(i).name));
-        
+    hgram = imhist(im, 65535);
+    
+    % calculate the background using morphological opening
     background = imopen(im,se);
+    
+    % extract the background from original image
     im = im - background + mean(background(:));
     
+    % equalize the histogram of corrected image to the original image
+    im = histeq(im,hgram);
+    
+    % normalize to 0-65535
     im = double(im);
     im = uint16(im - min(im(:)))*(65535 / (max(im(:)) - min(im(:))));
     imwrite(im, fullfile(output_dir, image_fnames(i).name));
