8caf9db357deca240d33fd7b5d43bcc70f59f218
jjahanip <jahandar.jahani@gmail.com>
2019-01-07 21:27:37 -0600

    switch to Tiff instead of imread/imwrite for consistency in image size

--- matlab/main.m
+++ matlab/main.m
@@ -3,7 +3,7 @@ clc; clear all; close all;
 tic
 input_dir = 'D:\Jahandar\Lab\images\50plex\stitched';
 output_dir = 'D:\Jahandar\Lab\images\50plex\IL_corrected';
-disk_size = 15;           % disk size for morphological opening
+disk_size = 20;           % disk size for morphological opening
 
 if isempty(gcp('nocreate'))
     myCluster = parcluster('local');
@@ -20,9 +20,11 @@ end
 se = strel('disk',disk_size);
 
 image_fnames = dir(fullfile(input_dir, '*.tif'));
-for i=1:size(image_fnames, 1)
+parfor i=1:size(image_fnames, 1)
+    
     % read image and get the histogram of the original image
-    im = imread(fullfile(input_dir, image_fnames(i).name));
+    t_in = Tiff(fullfile(input_dir, image_fnames(i).name), 'r+');
+    im = read(t_in);
     hgram = imhist(im, 65535);
     
     % calculate the background using morphological opening
@@ -37,8 +39,18 @@ for i=1:size(image_fnames, 1)
     % normalize to 0-65535
     im = double(im);
     im = uint16(im - min(im(:)))*(65535 / (max(im(:)) - min(im(:))));
-    imwrite(im, fullfile(output_dir, image_fnames(i).name));
     
+    % write image to disk
+    t_out = Tiff(fullfile(output_dir, image_fnames(i).name), 'w');
+    setTag(t_out, 'Photometric', getTag(t_in, 'Photometric'))
+    setTag(t_out, 'BitsPerSample', getTag(t_in, 'BitsPerSample'))
+    setTag(t_out, 'ImageLength', getTag(t_in, 'ImageLength'))
+    setTag(t_out, 'ImageWidth', getTag(t_in, 'ImageWidth'))
+    setTag(t_out, 'PlanarConfiguration', getTag(t_in, 'PlanarConfiguration'))
+    write(t_out,im);
+    
+    close(t_in);
+    close(t_out);
 end
 
 toc
